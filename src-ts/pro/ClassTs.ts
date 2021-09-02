ed775fbccc2985649e684d5cbfc75e478481c7b9
周润东 <zhourundong@jiudaotech.com>
2021-09-02 17:08:54 +0800

    加入接口、泛型

--- src-ts/pro/ClassTs.ts
+++ src-ts/pro/ClassTs.ts
@@ -18,6 +18,19 @@ class Cat extends Animal {
   constructor(name: string) {
     super(name);
     console.log('cat: ', this.name);
+    this.name = 0;
+  }
+
+  get name() {
+    return 'name';
+  }
+
+  set name(value: string) {
+    // name = value;
+  }
+
+  static shout() {
+    console.log(111);
   }
 
   sayHi() {
@@ -27,3 +40,24 @@ class Cat extends Animal {
 
 let smallCat = new Animal('Tom');
 smallCat.sayHi();
+Cat.shout();
+
+
+class Father{
+  public name;
+  protected constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Son extends Father{
+  constructor(public name) {
+    super(name);
+  }
+}
+
+let father = new Son('ha');
+
+
+
+
---
+++ src-ts/pro/DeneriscTs.ts
@@ -0,0 +1,39 @@
+export {};
+
+function createArr(length: number, value: any): Array<any> {
+  const res = [];
+  for (let i = 0; i < length; i++) {
+    res[i] = value;
+  }
+  return res;
+}
+
+// 泛形
+function createArr1<T, X> (length: number, value: T): Array<T> {
+  const res: T[] = [];
+  for (let i = 0; i < length; i++) {
+    res[i] = value;
+  }
+  return res;
+}
+
+interface LengthWise {
+  length: number;
+}
+
+// 泛形的约束
+function loggingIdentity<T extends LengthWise> (arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+// 泛型多参数约束
+function copyFields<T extends U, U>(target: T, source: U): T {
+  for (let o in source) {
+    target[o] = (<T>source)[o];
+  }
+  return target;
+}
+
+const x = { a: 1, b: 2, c: 3 };
+copyFields(x, { b: 10, c: 20 });
