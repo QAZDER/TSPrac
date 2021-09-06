27b7152465570ae5b0001bd3d877c3c4b8068930
周润东 <zhourundong@jiudaotech.com>
2021-09-06 11:18:59 +0800

    一些修改

--- src-ts/pro/DeneriscTs.ts
+++ src-ts/pro/DeneriscTs.ts
@@ -8,7 +8,10 @@ function createArr(length: number, value: any): Array<any> {
   return res;
 }
 
-// 泛形
+/**
+ * 泛型
+ * function functionName<T>(arg1: T): Array<T> {}
+ */
 function createArr1<T, X> (length: number, value: T): Array<T> {
   const res: T[] = [];
   for (let i = 0; i < length; i++) {
@@ -27,6 +30,8 @@ function loggingIdentity<T extends LengthWise> (arg: T): T {
   return arg;
 }
 
+loggingIdentity({ length: 9 });
+
 // 泛型多参数约束
 function copyFields<T extends U, U>(target: T, source: U): T {
   for (let o in source) {
@@ -38,6 +43,37 @@ function copyFields<T extends U, U>(target: T, source: U): T {
 const x = { a: 1, b: 2, c: 3 };
 copyFields(x, { b: 10, c: 20 });
 
-interface SearchFunc {
+interface SearchFunc<T> {
+  <T>(source: T, subString: T): boolean
+}
+
+let mySearchFunc: SearchFunc<string>;
+
+mySearchFunc = function(source: string, subString: string) {
+  return true;
+}
+
+
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount implements User{
+  name: string;
+  id: number;
+  value: string;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
 
 }
+
+const user: User = new UserAccount('Tom', 1);
+
+type result = true | false;
+
+
+
--- src-ts/pro/InterfaceTs.ts
+++ src-ts/pro/InterfaceTs.ts
@@ -1,3 +1,45 @@
 export {};
 
-class Point{}
+interface Alarm {
+  alert(): void;
+}
+
+// 接口继承另外的接口
+interface LightAlarm extends Alarm {
+  lightOn(): void;
+  lightOff(): void;
+}
+
+
+class ProAlarm implements LightAlarm {
+  alert() {
+    console.log('alert');
+  }
+
+  lightOff() {
+  }
+
+  lightOn() {
+  }
+}
+
+/**
+ * 接口可以继承类
+ * 当声明了一个类之后，可以同时将它作为类型使用
+ * p: Point, 这是因为在声明了类之后，同时会创建一个 Point 的类型
+ * 这个类型实际上与 PointInterface 等价
+ * 它不包含构造函数、静态属性、静态方法， 即只有实例属性、实例方法
+ * 因此当 interface extends class 时， 实际上继承的是类的实例类型
+ */
+interface AppleAlarm extends ProAlarm {
+
+}
+
+interface Backpack<Type> {
+  add: (obj: Type) => void;
+  get: () => Type;
+}
+
+declare const backpack: Backpack<string>;
+
+const obj = backpack.get();
